3d0f6dc560970849d412e4e257f6a98e2f84550f
docrinehart <sean.rinehart@headspring.com>
2015-07-13 13:47:29 -0500

    Add data-creation tool and initial resource class

---
+++ src/DeathStarImperator.Core/FakeDataPump.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeathStarImperator.Core
+{
+    public static class FakeDataPump
+    {
+        public static IList<Resource> InitResourceList()
+        {
+            var returnList = new List<Resource>();
+
+            returnList.Add(new Resource()
+            {
+                Name = "Storm Trooper",
+                Quantity = 345,
+                MaxQuantity = 7500,
+                PercentAdjustment = 0.00,
+                ProductionRate = 5.00                
+            });
+            returnList.Add(new Resource()
+            {
+                Name = "Tie Fighter",
+                Quantity = 27,
+                MaxQuantity = 1000,
+                PercentAdjustment = 5.00,
+                ProductionRate = 2.50
+            });
+            returnList.Add(new Resource()
+            {
+                Name = "Tie Fighter Adv.",
+                Quantity = 14,
+                MaxQuantity = 250,
+                PercentAdjustment = -25.0,
+                ProductionRate = 1.00
+            });
+            returnList.Add(new Resource()
+            {
+                Name = "Star Destroyer",
+                Quantity = 2,
+                MaxQuantity = 50,
+                PercentAdjustment = 0.00,
+                ProductionRate = 0.05
+            });
+
+            return returnList;
+        }  
+    }
+}
---
+++ src/DeathStarImperator.Core/Resource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeathStarImperator.Core
+{
+    public class Resource
+    {
+        public string Name { get; set; }
+        public double Quantity { get; set; }
+        public double MaxQuantity { get; set; }
+        public double ProductionRate { get; set; }
+        public string ProductionRateString {
+            get { return ProductionRate.ToString("+F3;-F3") + "/min"; }
+        }
+        public double PercentAdjustment { get; set; }
+        public string PercentAdjustmentString {
+            get { return PercentAdjustment.ToString("F2") + "%"; }
+        }
+
+        public Resource() { }
+    }
+}
